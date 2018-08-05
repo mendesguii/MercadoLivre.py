a7c93a33a94ef5b909c5638f2f93c949e047dad1
Guilherme Mendes <guilhermemendes789@gmail.com>
2018-08-05 13:31:24 -0300

    Bug fixes

--- Mercado_Livre.py
+++ Mercado_Livre.py
@@ -3,6 +3,7 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import re
 
 #Abre o arquivo
 csv_file = open('data.csv','a',newline='')
@@ -45,11 +46,11 @@ def Main():
 
             #quando o frete não é grátis
             if item_shop.find('p', class_="stack-item-info item--has-fulfillment") != None:
-                delivery = item_shop.find('p', class_="stack-item-info item--has-fulfillment").text
+                delivery = (item_shop.find('p', class_="stack-item-info item--has-fulfillment").text).replace(' ','',1)
             elif item_shop.find('p', class_="stack-item-info ") == None:
                 delivery = 'Combinar com o vendedor'
             else:
-                delivery = item_shop.find('p', class_="stack-item-info ").text
+                delivery = (item_shop.find('p', class_="stack-item-info ").text).replace(' ','',1)
 
             # Caso não apareça no "pré" anúncio as parcelas
             if item_shop.find('span',class_="item-installments-multiplier") == None:
@@ -59,42 +60,47 @@ def Main():
             else:
                 pre_text = 'Divide em'
                 cc_t = (item_shop.find('span',class_="item-installments-multiplier").text + 'de')
-                cc_price = item_shop.find('span', class_="item-installments-price").text
+                cc_price = (item_shop.find('span', class_="item-installments-price").text)
+
 
             #Caso não tenha a informação dos sem juros
             if (item_shop.find('span',class_="item-installments-interest") == None):
-                interest = ' com juros'
+                interest = 'com juros'
             else:
-                interest = item_shop.find('span',class_="item-installments-interest").text
+                interest = (item_shop.find('span',class_="item-installments-interest").text).replace(' ','',1)
 
             #quando não há informação nenhuma
             if (item_shop.find('div',class_="item__condition") == None):
                 sales_2 = ['0 vendidos','Sem Localização']
+
             #Quando não há vendas apenas a localização
             elif ' - ' not in (item_shop.find('div',class_="item__condition").text):
                 sales_2 = ['0 vendidos',item_shop.find('div',class_="item__condition").text]
+
             #quando não tem frete grátis e tem vendas
             elif item_shop.find('p', class_="stack-item-info item__free-shipping-disabled") != None:
                 if item_shop.find('p', class_="stack-item-info item__free-shipping-disabled").text == ' Envio para todo o país  ':
+                    sales = (item_shop.find('div', class_="item__condition").text).replace(' ', '', 1)
                     sales_2=sales.split("-")
+
                 else:
                     sales_2 = ['0',item_shop.find('p', class_="stack-item-info item__free-shipping-disabled").text]
             else:
-                sales = item_shop.find('div',class_="item__condition").text
+                sales = (item_shop.find('div',class_="item__condition").text).replace(' ','',1)
                 sales_2=sales.split("-")
 
             count += 1
-
+            sales_2[1]=sales_2[1].replace(' ', '', 1)
             #Printando resultado
             print('Anúncio: ' + name_item)
-            print('Preço R$ ' + price_item +',' + price_d_item)
+            print('Preço: R$ ' + price_item +',' + price_d_item)
             print(pre_text + cc_t + cc_price + interest)
             print('Frete: ' + delivery)
             print('Link: ' +link)
-            print('Vendas:'+ sales_2[0])
-            print('Localização:' + sales_2[1])
+            print('Vendas: '+ sales_2[0])
+            print('Localização: ' + sales_2[1])
             print('\n')
-            csv_writer.writerow([name_item,price_item+','+price_d_item,cc_price,interest,delivery,sales_2[0],sales_2[1],link])
+            csv_writer.writerow([name_item,('R$ '+price_item+','+price_d_item),cc_price,interest,delivery,sales_2[0],sales_2[1],link])
 
     print('Foram extraídos '+str(count)+' anúncios.')
     csv_file.close
